3aec917de787d3efbaf9dbe7f4ceae9176e16695
darknessmalware <kozoderovaroslav@gmail.com>
2026-02-15 00:55:23 +0100

    collect all and shop

--- main.lua
+++ main.lua
@@ -1,9 +1,15 @@
+-- TODO: 
+--	Auto Equip The Drill (DrillToggle)
+--	Auto Rebirth
+-- 	Local Player Settings (WalkSpeed, JumpHeight, etc)
+
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
+   Name = "Drill for Ore Script by darkness",
    Icon = 0,
-   ShowText = "Rayfield",
+   ShowText = "",
    Theme = "DarkBlue",
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility
@@ -12,8 +18,31 @@ local Window = Rayfield:CreateWindow({
    DisableBuildWarnings = false
 })
 
+local Drills = {}
+local HandDrills = {}
+local DrillToBuy = ""
+local HandDrillToBuy = ""
+
+function GetHandDrills()
+		for _, item in ipairs(game:GetService("ReplicatedStorage").HandDrills:GetChildren()) do 
+		table.insert(HandDrills, 1, item.Name)
+		print(item.Name)
+	end
+	return HandDrills
+	
+end
+
+function GetDrills()
+	for _, item in ipairs(game:GetService("ReplicatedStorage").Drills:GetChildren()) do 
+		table.insert(Drills, 1, item.Name)
+		print(item.Name)
+	end
+	return Drills
+end
+
 local DrillValue = false
 local SellAllValue = false
+local CollectAllValue = false
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
 local Main = Window:CreateTab("Main")
@@ -36,7 +65,7 @@ local DrillToggle = Main:CreateToggle({
 	end,
 })
 
-local SellAllToggle = Shop:CreateToggle({
+local SellAllToggle = Main:CreateToggle({
 	Name = "Sell All",
 	CurrentValue = false,
 	Callback = function(Value)
@@ -49,5 +78,60 @@ local SellAllToggle = Shop:CreateToggle({
 				wait(5)
 			end
 		end
+	end,
+})
+
+local CollectAllToggle = Main:CreateToggle({
+	Name = "Collect All",
+	CurrentValue = false,
+	Callback = function(Value)
+		CollectAllValue = Value
+		if Value then
+			while CollectAllValue do
+				for _, v in pairs(workspace.Plots:GetDescendants()) do
+					if v.Name:lower():find("vault") then
+						game:GetService("ReplicatedStorage").Packages.Knit.Services.PlotService.RE.CollectDrill:FireServer(v)
+					end
+				end
+				wait(10)
+			end
+		end
+	end,
+})
+
+local Drills = GetDrills()
+local HandDrills = GetHandDrills()
+
+local HandDrillsDropdown = Shop:CreateDropdown({
+   Name = "Hand Drills",
+   Options = HandDrills,
+   CurrentOption = HandDrills[1],
+   MultipleOptions = false,
+   Callback = function(Option)
+		HandDrillToBuy = Option[1]
+   end,
+})
+
+local BuyDrillButton = Shop:CreateButton({
+	Name = "Buy Hand Drill",
+	Callback = function ()
+		game:GetService("ReplicatedStorage").Packages.Knit.Services.OreService.RE.BuyDrill:FireServer(HandDrillToBuy)
+	end,
+})
+
+local DrillsDropdown = Shop:CreateDropdown({
+   Name = "Drills",
+   Options = Drills,
+   CurrentOption = Drills[1],
+   MultipleOptions = false,
+   Callback = function(Option)
+		DrillToBuy = Option[1]
+   end,
+})
+
+local BuyDrillButton = Shop:CreateButton({
+	Name = "Buy Drill",
+	Callback = function ()
+		game:GetService("ReplicatedStorage").Packages.Knit.Services.OreService.RE.BuyDrill:FireServer(DrillToBuy)
 	end,
 })
